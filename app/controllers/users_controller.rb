d722fc5257063e1f50b1ae2385b15588712b2c7c
Laurent Jacquot <laurentjacquot1@yahoo.com>
2013-05-09 13:59:08 +0200

    Finish the signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,21 @@
 class UsersController < ApplicationController
+
+  def show
+  	# raise params.inspect
+  	@user = User.find(params[:id])
+  end
+
   def new
+  	@user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to SampleApp #{ @user.name }"
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 end
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name "Thanh Do" 
+    email "td@aol.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
